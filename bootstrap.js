4ad60adc3787f73e4c6c1cf4f0030a1b55bc3d25
Abrahan Gonzalez <trighhorn@gmail.com>
2020-02-13 14:41:45 -0700

    finished lottery

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,19 @@
-const weights = {
-  winning: 1,
-  losing: 9
-}
+const weightedLottery = weights => {
+  let containerArray = [];
 
-weightedLottery(weights)
+  Object.keys(weights).forEach(key => {
+    for (let i = 0; i < weights[key]; i++) {
+      containerArray.push(key);
+    }
+  });
+
+  return containerArray[(Math.random() * containerArray.length) | 0];
+};
+
+const weights = {
+  green: 1,
+  yellow: 2,
+  red: 3
+};
+
+weightedLottery(weights);
